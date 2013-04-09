b7d0df2173674dba69a3e3e66453ef440117b1fb
Elijah Insua <tmpvar@gmail.com>
2013-04-09 16:45:04 -0700

    test es add behavior

--- test/test.js
+++ test/test.js
@@ -5,7 +5,16 @@ var spawn = require('child_process').spawn,
     request = require('request'),
     TMP_DIR = __dirname + '/tmp',
     seq = require('../lib/seq'),
+    elasticsearch = require('../lib/elasticsearch'),
+    http = require('http'),
     assert = require('assert');
+
+
+
+var proc = {
+  es : null,
+  couch : null
+};
 
 describe('npm2es', function() {
   describe('sequence', function() {
@@ -122,6 +131,143 @@ describe('npm2es', function() {
     });
 
     // TODO: ensure the sequence is updated as new docs come in...
+  });
+});
+
+describe('elasticsearch', function() {
+  describe('#add', function() {
+
+    describe('skip behavior', function() {
+      it('should skip docs without an id', function(done) {
+
+        var called = 0;
+        var server = http.createServer(function(req, res) {
+          res.writeHead(200);
+          res.end('{}');
+          called++;
+        });
+        server.listen(9999, function(e) {
+          assert.ok(!e);
+
+          elasticsearch.add('http://localhost:9999/npm', { _id : 'test' }, function(err) {
+            server.close(done);
+            assert.ok(err);
+            assert.equal(err.message, 'SKIP: test');
+            assert.equal(called, 0)
+          });
+        });
+      });
+
+
+      it('should skip if the version matches', function(done) {
+
+        var called = 0;
+        var server = http.createServer(function(req, res) {
+          res.writeHead(200);
+          res.end(JSON.stringify({
+            _source: {
+              version : "0.1.0"
+            }
+          }));
+          called++;
+        });
+
+        server.listen(9999, function(e) {
+          assert.ok(!e);
+
+          var payload =  {
+            _id : 'test',
+            name : 'test',
+            "dist-tags" : {
+              latest: "0.1.0"
+            },
+            versions : {
+              "0.1.0" : {
+                _npmUser: { name : 'tmpvar' }
+              }
+            }
+          };
+
+          elasticsearch.add('http://localhost:9999/npm', payload, function(err) {
+            server.close(done);
+            assert.ok(err);
+            assert.equal(err.message, 'SKIP VERSION: test');
+            assert.equal(called, 1)
+          });
+        });
+      });
+
+
+      it('should skip docs that normalize fails on', function(done) {
+        var called = 0;
+        var server = http.createServer(function(req, res) {
+          res.writeHead(200);
+          res.end('{}');
+          called++;
+        });
+        server.listen(9999, function(e) {
+          assert.ok(!e);
+
+          var payload = { _id : 'test', name: 'test' }
+          elasticsearch.add('http://localhost:9999/npm', payload, function(err) {
+            server.close(done);
+            assert.ok(err);
+            assert.equal(err.message, 'SKIP: test');
+            assert.equal(called, 0)
+          });
+        });
+      });
+
+      it('should merge over top level keys of the existing document', function(done) {
+        var server = http.createServer(function(req, res) {
+
+          var method = req.method.toLowerCase();
+          console.log('REQ', method);
+          if (method === 'get') {
+            res.writeHead(200);
+            res.end(JSON.stringify({
+              _source : {
+                name : "test",
+                extra: "keep me"
+              }
+            }));
+          }
+
+          if (method === 'put') {
+            var data = '';
+            req.on('data', function(chunk) {
+              data += chunk.toString();
+            });
+            req.on('end', function() {
+              var obj = JSON.parse(data);
+              assert.equal(obj.extra, 'keep me');
+              res.writeHead(200);
+              res.end();
+              server.close(done);
+            });
+          }
+        });
+
+        server.listen(9999, function(e) {
+          assert.ok(!e);
+
+          var payload =  {
+            _id : 'test',
+            name : 'test',
+            "dist-tags" : {
+              latest: "0.1.0"
+            },
+            versions : {
+              "0.1.0" : {
+                _npmUser: { name : 'tmpvar' }
+              }
+            }
+          };
+          elasticsearch.add('http://localhost:9999/npm', payload);
+        });
+      });
+
+    });
   });
 });
 
