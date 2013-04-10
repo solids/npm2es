d77081e8c73b08fb1ae9d994dc18bd573bde23cc
Elijah Insua <tmpvar@gmail.com>
2013-04-09 20:54:32 -0700

    remove _all_docs logic branch and dependencies

--- bin/npm2es.js
+++ bin/npm2es.js
@@ -5,96 +5,65 @@ if (!argv.couch || !argv.es) {
   return console.log('USAGE: npm2es --couch="<url to couch>" --es="<url to elasticsearch>"');
 }
 
-var request = require('request'),
-    follow = require('follow'),
+var follow = require('follow'),
     normalize = require('npm-normalize'),
-    JSONStream = require('JSONStream'),
-    through = require('through'),
     seq = require('../lib/seq'),
     elasticsearch = require('../lib/elasticsearch'),
-    since = argv.since || 0;
+    since = argv.since;
 
-if (!since) {
-  since = seq.load();
+if (typeof since === 'undefined') {
+  since = seq.load() || 0;
 }
 
-if (!since) {
-  request({
-    url: argv.couch,
-    json: true
-  }, function(e,r,obj) {
-    since = obj.update_seq;
-    indexAllPackages();
-  });
-} else {
-  beginFollowing();
-}
+console.log('BEGIN FOLLOWING @', since);
 
-function indexAllPackages() {
-  // Get all docs
-  var stream = request.get(argv.couch + '/_all_docs?include_docs=true');
-  stream.pipe(JSONStream.parse('rows.*.doc')).pipe(through(function(doc) {
-    this.pause()
-    elasticsearch.add(argv.es, doc, function(e, o) {
+var last = since;
+follow({
+  db: argv.couch,
+  since: since,
+  include_docs: true
+},  function(err, change) {
+
+  var that = this;
+  this.pause();
+
+  if (err) {
+    return console.error('ERROR', err);
+  }
+
+  if (!change.id) {
+    return console.log('SKIP', change);
+  }
+
+  if (last + 1000 < change.seq) {
+    last = change.seq;
+    seq.save(last, function(e) {
       if (e) {
-        console.log(e.message);
+        console.error('ERROR', 'could not save latest sequence');
+      }
+    });
+  }
+
+  // Remove the document elasticsearch
+  if (change.deleted) {
+    elasticsearch.remove(argv.es, change.id, function(err) {
+      if (!err) {
+        console.log('DELETED', change.id);
+      } else {
+        console.error('ERROR', 'could not delete document', err);
+      }
+      that.resume();
+    });
+
+  // Add the doument to elasticsearch
+  } else {
+    elasticsearch.add(argv.es, change.doc, function(e, o) {
+      if (e) {
+        console.error(e.message);
       } else {
         console.log('ADD', o.name);
       }
-
-      this.resume();
-    }.bind(this));
-  }));
-
-  stream.on('end', function() {
-    seq.save(since)
-    beginFollowing();
-  });
-};
-
-function beginFollowing() {
-  console.log('BEGIN FOLLOWING @', since);
-
-  var last = since;
-  follow({
-    db: argv.couch,
-    since: since,
-    include_docs: true
-  },  function(err, change) {
-
-    if (err) {
-      return console.error('ERROR', err);
-    }
-
-    if (!change.id) {
-      return console.log('SKIP', change);
-    }
-
-    if (last + 1000 < change.seq) {
-      last = change.seq;
-      seq.save(last);
-    }
-
-    // Remove the document elasticsearch
-    if (change.deleted) {
-      elasticsearch.remove(argv.es, change.id, function(err) {
-        if (!err) {
-          console.log('DELETED', change.id);
-        } else {
-          console.error('ERROR', 'could not delete document', err);
-        }
-      });
-
-    // Add the doument to elasticsearch
-    } else {
-      elasticsearch.add(argv.es, change.doc, function(e, o) {
-        if (e) {
-          console.error(e.message);
-        } else {
-          console.log('ADD', o.name);
-        }
-      });
-    }
-  });
-};
-
+      that.resume();
+    });
+  }
+});
