b4aa97eb79ffd3181f876bc9f683429ee7fa4487
Elijah Insua <tmpvar@gmail.com>
2013-04-13 13:44:16 -0700

    use elasticsearch to store the sequence (<argv.index>/config/sequence)

--- bin/npm2es.js
+++ bin/npm2es.js
@@ -2,7 +2,7 @@
 
 var argv = require('optimist').argv;
 if (!argv.couch || !argv.es) {
-  return console.log('USAGE: npm2es --couch="<url to couch>" --es="<url to elasticsearch>" [--seq="/path/to/.seq"] [--interval=1000]');
+  return console.log('USAGE: npm2es --couch="<url to couch>" --es="<url to elasticsearch>" [--interval=1000]');
 }
 
 var follow = require('follow'),
@@ -12,118 +12,134 @@ var follow = require('follow'),
     path = require('path'),
     extend = require('extend'),
     since = argv.since,
-    interval = argv.interval || 1000,
-    seqFile = argv.seq || path.join(process.env.HOME,'.npm2es-seq');
+    interval = argv.interval || 1000;
 
 if (typeof since === 'undefined') {
-  try {
-    var contents = fs.readFileSync(seqFile).toString();
-    since = parseInt(contents, 10);
-    // TODO: NaN, non-existant file, etc
-  } catch (e) {
-    since = 0;
-  }
+  request.get({
+    url : argv.es + '/config/sequence',
+    json: true
+  }, function(e, r, o) {
+
+    if (!e && o && o._source && o._source.value) {
+      since = o._source.value;
+    } else {
+      since = 0;
+    }
+    beginFollowing();
+  });
+
 } else {
-  fs.writeFileSync(seqFile, since);
+  request.put({
+    url : argv.es + '/config/sequence',
+    json : {
+      value: since
+    }
+  }, beginFollowing);
 }
 
-console.log('BEGIN FOLLOWING @', since);
+function beginFollowing() {
+  console.log('BEGIN FOLLOWING @', since);
+  var last = since;
+  follow({
+    db: argv.couch,
+    since: since,
+    include_docs: true
+  },  function(err, change) {
+    if (err) {
+      return console.error('ERROR:', err.message, argv.couch);
+    }
 
-var last = since;
-follow({
-  db: argv.couch,
-  since: since,
-  include_docs: true
-},  function(err, change) {
-  if (err) {
-    return console.error('ERROR:', err.message, argv.couch);
-  }
-
-  if (!change) {
-    return;
-  }
-
-  var that = this;
-  this.pause();
-
-  if (!change.id) {
-    return console.log('SKIP', change);
-  }
-
-  if (last + interval < change.seq) {
-    last = change.seq;
-    fs.writeFile(seqFile, last, function(e) {
-      if (e) {
-        console.error('ERROR', 'could not save latest sequence');
-      }
-
-      console.log('SYNC', last);
-    });
-  }
-
-  // Remove the document elasticsearch
-  if (change.deleted) {
-    request.del(argv.es + '/package/' + change.id, function(err) {
-      if (!err) {
-        console.log('DELETED', change.id);
-      } else {
-        console.error('ERROR', 'could not delete document', err);
-      }
-      that.resume();
-    });
-
-  // Add the doument to elasticsearch
-  } else {
-
-    var p = normalize(change.doc);
-
-    if (!p || !p.name) {
-      console.log('SKIP: ' + change.doc._id);
-      that.resume();
+    if (!change) {
       return;
     }
 
-    request.get({
-      url: argv.es + '/package/' + p.name,
-      json: true
-    }, function(e,b, obj) {
+    var that = this;
+    this.pause();
 
-      // follow gives us an update of the same document 2 times
-      // 1) for the actual package.json update
-      // 2) for the tarball
-      // skip a re-index for #2
-      if (!e && obj && obj._source && obj._source.version === p.version) {
-        console.log('SKIP VERSION: ' + change.doc._id);
+    if (!change.id) {
+      return console.log('SKIP', change);
+    }
+
+    if (last + interval < change.seq) {
+      last = change.seq;
+      request.put({
+        url : argv.es + '/config/sequence',
+        json : {
+          value: last
+        }
+      }, function(e, r, o) {
+        if (e || !o.ok) {
+          return console.error('ERROR', 'could not save latest sequence');
+        }
+
+        console.log('SYNC', last);
+      });
+    }
+
+    // Remove the document from elasticsearch
+    if (change.deleted) {
+      request.del(argv.es + '/package/' + change.id, function(err) {
+        if (!err) {
+          console.log('DELETED', change.id);
+        } else {
+          console.error('ERROR', 'could not delete document', err);
+        }
         that.resume();
-      } else {
-        obj = obj || {};
+      });
 
-        if (p.dependencies) {
-          p.dependencies = Object.keys(p.dependencies);
-        }
+    // Add the document to elasticsearch
+    } else {
 
-        if (p.devDependencies) {
-          p.devDependencies = Object.keys(p.devDependencies);
-        }
+      var p = normalize(change.doc);
 
-        if (p.time) {
-          delete p.time;
-        }
-
-        request.put({
-          url: argv.es + '/package/' + p.name,
-          json: extend(obj._source || {}, p)
-        }, function(e, r, b) {
-          if (e) {
-            console.error(e.message);
-          } else if (b.error) {
-            console.error(b.error);
-          } else {
-            console.log('ADD', p.name, r.statusCode);
-          }
-          that.resume();
-        });
+      if (!p || !p.name) {
+        console.log('SKIP: ' + change.doc._id);
+        that.resume();
+        return;
       }
-    });
-  }
-});
+
+      request.get({
+        url: argv.es + '/package/' + p.name,
+        json: true
+      }, function(e,b, obj) {
+
+        // follow gives us an update of the same document 2 times
+        // 1) for the actual package.json update
+        // 2) for the tarball
+        // skip a re-index for #2
+        if (!e && obj && obj._source && obj._source.version === p.version) {
+          console.log('SKIP VERSION: ' + change.doc._id);
+          that.resume();
+        } else {
+          obj = obj || {};
+
+          if (p.dependencies) {
+            p.dependencies = Object.keys(p.dependencies);
+          }
+
+          if (p.devDependencies) {
+            p.devDependencies = Object.keys(p.devDependencies);
+          }
+
+          if (p.time) {
+            delete p.time;
+          }
+
+          request.put({
+            url: argv.es + '/package/' + p.name,
+            json: extend(obj._source || {}, p)
+          }, function(e, r, b) {
+            if (e) {
+              console.error(e.message);
+            } else if (b.error) {
+              console.error(b.error);
+            } else {
+              console.log('ADD', p.name, r.statusCode);
+            }
+            that.resume();
+          });
+        }
+      });
+    }
+  });
+}
